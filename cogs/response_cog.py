b68b2467157e8095298042979f57f581bd30b673
Drew <drew@bnimble.org>
2022-03-03 23:11:01 -0800

    more cha t work

--- cogs/response_cog.py
+++ cogs/response_cog.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+from .shared import vc, utils
 import discord
 from discord.ext import commands
 
@@ -30,8 +30,11 @@ class Responses(commands.Cog):
             await message.channel.send('Fuck you!')
         elif message.content.lower().__contains__('fuck'):
             await message.channel.send('me?')
+
         if message.content.lower().__contains__('gn'):
             await message.channel.send('gn honey bunches :heart_eyes:')
+        if message.content.lower().startswith('call me daddy'):
+            await message.channel.send('daddy :tired_face:')
 
         if message.content.lower().startswith('?'):
             rand = random.randint(0, 100)
@@ -43,7 +46,7 @@ class Responses(commands.Cog):
                 await message.channel.send('Are you blind?')
             if rand == 100:
                 await message.channel.send('You are a coward, and history will forget you')
-        if message.content.lower().__contains__('wucka') or message.content.lower().startswith('woka'):
+        if message.content.lower().__contains__('wucka') or message.content.lower().__contains__('woka'):
             rand = random.randint(0,101)
             if rand < 20:
                 await message.channel.send('What.')
@@ -59,8 +62,16 @@ class Responses(commands.Cog):
                 await message.channel.send('I\'m busy with your mom, give me a sec')
             if 90 <= rand < 100:
                 await message.channel.send(':|')
+                await vc.join_and_play(message.author.voice.channel, "sounds/ugly.mp3")
             if rand == 100:
                 await message.channel.send('YOU DARE SUMMON ME?')
             if rand == 101:
                 await message.channel.send('https://cdn.discordapp.com/attachments/420073745651335188/948279662755323974/unknown.png')
                 await message.channel.send('This is how I feel about you.')
+        if message.content.lower().startswith('yeah you'):
+            await message.channel.send(':(')
+        if message.content.lower().__contains__('uninstall'):
+            await message.channel.send('I gotta uninstall my dick from your mom first')
+        if message.content.lower().__contains__('creeper'):
+            await vc.join_and_play(message.author.voice.channel, "sounds/creeper.mp3")
+
